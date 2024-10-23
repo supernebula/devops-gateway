69d3453cf38544a3599186a12ebbd12198a6247c
Administrator <admin@example.com>
2024-10-23 12:45:47 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-exec java $JAVA_OPTS -jar /home/app.jar --spring.config.location=/home/config/bootstrap.properties
+exec java $JAVA_OPTS -jar /home/app.jar -Dspring.config.location=/home/config/application.yml
 #exec java $JAVA_OPTS -jar /home/app.jar
